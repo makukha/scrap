e5d8531eccab0c8f1efc7e3812014c43f1772ac4
Michael.Makukha <m.makukha@gmail.com>
2025-05-31 17:48:19 +0300

    Add projects handling

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -1,17 +1,51 @@
-# encoding: utf-8
 from doctest import ELLIPSIS
+import subprocess
+import sys
 from unittest import TestCase
 
 from doctestcase import doctestcase
 
 
-case = doctestcase(options=ELLIPSIS)
-
-
-@case
-class UsageFirst(TestCase):
+@doctestcase(options=ELLIPSIS)
+class UsageProjectEntrypoint(TestCase):
     """
-    First use case
+    Export project with package entrypoint
 
-    >>> from scrap import __version__
+    When project package declares entry point `scrap.project`,
+    ```python
+    # myproject/myproject.py
+    import scrap
+
+    class MyProject(scrap.Project):
+        ...
+    ```
+    ```toml
+    # myproject/pyproject.toml
+    [project]
+    name = "myproject"
+    version = "0.1.0"
+    description = ""
+
+    [project.entry-points."scrap.project"]
+    myproject = "myproject:MyProject"
+    ```
+
+    it becomes registered as `scrap` project:
+
+    >>> import scrap
+    >>> scrap.get_projects()
+    (('myproject', <class 'myproject.MyProject'>),)
     """
+
+    pkg_path = 'tests/usage/myproject'
+    pkg_name = 'myproject'
+
+    def setUp(self) -> None:
+        subprocess.check_call(
+            [sys.executable, '-m', 'pip', 'install', self.pkg_path]
+        )
+
+    def tearDown(self) -> None:
+        subprocess.check_call(
+            [sys.executable, '-m', 'pip', 'uninstall', '-y', self.pkg_name]
+        )
---
+++ tests/usage/myproject/myproject.py
@@ -0,0 +1,4 @@
+import scrap
+
+class MyProject(scrap.Project):
+    ...
